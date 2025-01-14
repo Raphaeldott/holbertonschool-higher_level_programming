0bba583f75601d4e9675dd69f23a2bcf5a657ed6
raphaeldott <raphael.dott@hotmail.com>
2025-01-14 11:52:46 +0100

    fastcommit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end="")
-        else:
-            print(char, end="")
-    print()
+    print("{}".format("".join([
+        chr(ord(char) - 32) if ord('a') <= ord(char) <= ord('z') else char
+        for char in str
+    ])))
