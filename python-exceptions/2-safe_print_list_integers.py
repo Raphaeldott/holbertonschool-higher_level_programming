81f7551b77943a75e000ce6163c592151598dc6e
raphaeldott <raphael.dott@hotmail.com>
2025-01-22 12:05:02 +0100

    fastcommit

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (IndexError, ValueError, TypeError):
+            pass
+    print()  # To print a new line after all elements
+    return count
