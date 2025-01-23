7fbf18031fbbaba73689a0664e1be12405ed7438
raphaeldott <raphael.dott@hotmail.com>
2025-01-23 15:24:38 +0100

    fastcommit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats after casting them to integers.
+
+    Args:
+        a: The first integer or float.
+        b: The second integer or float, default is 98.
+
+    Returns:
+        The addition of a and b as an integer.
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
