36e6ba9664bde547ab469304276c1fd80fcf2b1e
raphaeldott <raphael.dott@hotmail.com>
2025-01-25 13:37:27 +0100

    fastcommit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+'''This modules provides a function to perform
+    integer addition.
+    Args:
+        a: The first integer or float.
+        b: The second integer or float, default is 98.
+
+    Returns:
+        The addition of a and b as an integer.
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+'''
+
 def add_integer(a, b=98):
     """
-    Adds two integers or floats after casting them to integers.
-
     Args:
         a: The first integer or float.
         b: The second integer or float, default is 98.
