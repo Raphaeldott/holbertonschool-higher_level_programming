b311193fbdaf5bca2fd09e14ef20e27934ea6375
raphaeldott <raphael.dott@hotmail.com>
2025-02-04 10:34:28 +0100

    fastcommit

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from 9-rectangle import Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 """
 Square class that inherits from Rectangle.
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from 9-rectangle import Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
+
 """
 task 11
 """
