ed377f1be7ddb30dce7893af0b20857a42e87c69
raphaeldott <raphael.dott@hotmail.com>
2025-02-10 14:11:40 +0100

    fastcommit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,8 @@ write in a file
 
 
 def write_file(filename="", text=""):
+    '''
+    writing inside a file
+    '''
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
