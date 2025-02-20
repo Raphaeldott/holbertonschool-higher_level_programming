99a3ddd8e0f5f96147552bf7248f7f9a8b79662a
raphaeldott <raphael.dott@hotmail.com>
2025-02-20 13:14:34 +0100

    fastcommit

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -34,8 +34,6 @@ def add_user():
     username = data.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
-    if username in users:
-        return jsonify({"error": "User already exists"}), 400
     users.clear()
     users[username] = data
     return jsonify({"message": "User added", "user": data}), 201
