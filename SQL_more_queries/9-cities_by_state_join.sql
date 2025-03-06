ce66d8d2ee9095641278d9319f0af356c1d7c851
raphaeldott <raphael.dott@hotmail.com>
2025-03-06 10:09:08 +0100

    SQL_more_queries reload

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id AS id, cities.name AS name, states.name AS name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
