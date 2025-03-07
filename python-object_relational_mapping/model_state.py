ecc2793afee37dd26eaaacb345fd961c6c1cdc3c
raphaeldott <raphael.dott@hotmail.com>
2025-03-07 17:25:57 +0100

    add shebang

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,16 @@
-from sqlalchemy import Column, Integer, String, create_engine
+#!/usr/bin/python3
+"""Class definition for State and linking it to the states table in MySQL"""
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Definition of the State class, inheriting from Base."""
+    """State class represents the 'states' table in the MySQL database"""
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa')
-    Base.metadata.create_all(engine)
