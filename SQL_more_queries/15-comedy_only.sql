b6afc50a8dafc4ef89b65b14140ff3c2d3f07712
raphaeldott <raphael.dott@hotmail.com>
2025-03-07 10:16:19 +0100

    removed error synthax in task16 en changed show tto shows in task15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all comedy shows in the database --
 SELECT s.title
-FROM tv_show s
+FROM tv_shows s
 JOIN tv_show_genres sg ON s.id = sg.show_id
 JOIN tv_genres g ON sg.genre_id = g.id
 WHERE g.name = 'Comedy'
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT s.title, g.name
 FROM tv_shows s
 LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
 LEFT JOIN tv_genres g ON sg.genre_id = g.id
-ORDER BY s.title ASC g.name ASC;
+ORDER BY s.title ASC, g.name ASC;
